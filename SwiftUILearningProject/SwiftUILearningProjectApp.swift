e3a8e7d8bae5b046dd3256bc91deafb90b4165a0
MartinK <martin.kusek1@gmail.com>
2022-11-10 17:52:45 +0100

    Initial Commit

---
+++ SwiftUILearningProject/SwiftUILearningProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUILearningProjectApp.swift
+//  SwiftUILearningProject
+//
+//  Created by Martin Kusek on 10.11.2022..
+//
+
+import SwiftUI
+
+@main
+struct SwiftUILearningProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
